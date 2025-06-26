dad010d8121a90260a7d50f87e6f58d225e28d35
Victor Diez <victordiez1@gmail.com>
2025-06-26 18:44:48 +0200

    Search methode array

--- scripts/search.js
+++ scripts/search.js
@@ -1,22 +1,3 @@
-// const searchBar = document.querySelector(".search");
-// const resultsList = document.getElementById("recipe-container");
-// const items = resultsList.getElementsByClassName("card");
-
-// searchBar.addEventListener("input", () => {
-//   const searchTerm = searchBar.value.toLowerCase();
-// if (searchTerm.length > 2) {
-//   for (let i = 0; i < items.length; i++) {
-//     const itemText = items[i].textContent.toLowerCase();
-//     if (itemText.includes(searchTerm)) {
-//       items[i].style.display = ""; // montrer l'élément
-//     } else {
-//       items[i].style.display = "none"; // cacher l'élément
-//     }
-//   }}
-// });
-
-import recipes from './recipe.js'
-
 const searchBar = document.querySelector(".search");
 const resultsList = document.getElementById("recipe-container");
 const items = Array.from(resultsList.getElementsByClassName("card"));
